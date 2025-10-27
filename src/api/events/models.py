3aefe621f2e35ee3317d3bc35e9333473509880c
dawid mrotek <DawidMrotek@protonmail.ch>
2025-10-27 22:13:22 +0100

    [ADD] datetime field

--- src/api/events/models.py
+++ src/api/events/models.py
@@ -1,12 +1,23 @@
+from datetime import datetime, timezone
 from typing import List, Optional
+import sqlmodel
 from sqlmodel import SQLModel, Field
 
+
+
+def get_utc_now():
+    return datetime.now(timezone.utc).replace(tzinfo=timezone.utc)
 
 
 class EventModel(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     page: Optional[str] = ""
     description: Optional[str] = ""
+    created_at: datetime = Field(
+        default_factory=get_utc_now,
+        sa_type=sqlmodel.DateTime(timezone=True),
+        nullable=False
+    )
 
 
 class EventCreateModel(SQLModel):
