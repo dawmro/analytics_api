120a356de6fb8b783706f929c2a2165fad6e0653
dawid mrotek <DawidMrotek@protonmail.ch>
2025-10-28 03:52:13 +0100

    [UPDATE] SQLModel to TimescaleModel and Hypertable

--- src/api/db/session.py
+++ src/api/db/session.py
@@ -1,19 +1,21 @@
 import sqlmodel
+import timescaledb
 from sqlmodel import SQLModel, Session
-from .config import DATABASE_URL
+from .config import DATABASE_URL, DB_TIMEZONE
 
 
 
 if DATABASE_URL == "":
     raise NotImplementedError("DATABASE_URL missing!")
 
-engine = sqlmodel.create_engine(DATABASE_URL)
+engine = timescaledb.create_engine(DATABASE_URL, timezone=DB_TIMEZONE)
 
 
 def init_db():
     print("creating database")
     SQLModel.metadata.create_all(engine)
-
+    print("creating hypertables")
+    timescaledb.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
